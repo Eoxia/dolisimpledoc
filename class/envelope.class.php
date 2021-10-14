504ac5d6335d449053d3858e7c82bb7ec938941f
Nicolas <nicolas@eoxia.com>
2021-10-14 15:53:59 +0200

    [Img] fix: 32px for picto

--- class/envelope.class.php
+++ class/envelope.class.php
@@ -58,7 +58,7 @@ class Envelope extends CommonObject
 	/**
 	 * @var string String with name of icon for document. Must be the part after the 'object_' into object_document.png
 	 */
-	public $picto = 'doliletter@doliletter';
+	public $picto = 'doliletter32px@doliletter';
 
 	/**
 	 *  'type' field format ('integer', 'integer:ObjectClass:PathToClass[:AddCreateButtonOrNot[:Filter]]', 'sellist:TableName:LabelFieldName[:KeyFieldName[:KeyFieldParent[:Filter]]]', 'varchar(x)', 'double(24,8)', 'real', 'price', 'text', 'text:none', 'html', 'date', 'datetime', 'timestamp', 'duration', 'mail', 'phone', 'url', 'password')
