7f1a48403ab0dbb4d0a4f4fc78e9140203fc2105
Théo David <theo.evarisk@gmail.com>
2022-04-07 16:33:20 +0200

    [Envelope] fix: enhance content size in create action

--- envelope_card.php
+++ envelope_card.php
@@ -1028,7 +1028,7 @@ if ($action == 'create') {
 
 	//Content -- Contenu
 	print '<tr class=""><td><label for="content">'.$langs->trans("Content").'</label></td><td>';
-	$doleditor = new DolEditor('content', GETPOST('content'), '', 150, 'dolibarr_details', '', false, true, $conf->global->FCKEDITOR_ENABLE_SOCIETE, ROWS_3, '90%');
+	$doleditor = new DolEditor('content', GETPOST('content'), '', 250, 'dolibarr_details', '', false, true, $conf->global->FCKEDITOR_ENABLE_SOCIETE, ROWS_3, '90%');
 	$doleditor->Create();
 	print '</td></tr>';
 
