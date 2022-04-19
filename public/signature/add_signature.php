4e5946b8e5dfa8b4e73048f851c155e5bffa0cce
Théo David <theo.evarisk@gmail.com>
2022-04-19 11:41:05 +0200

    [PublicInterface] add: check when conf is activated but document is not shared

--- public/signature/add_signature.php
+++ public/signature/add_signature.php
@@ -158,19 +158,21 @@ if (dol_strlen($element->signature)) {
 			<?php if ($conf->global->DOLILETTER_SHOW_DOCUMENTS_ON_PUBLIC_INTERFACE) : ?>
 				<div class="wpeo-gridlayout grid-2 file-generation">
 					<?php if ($type == 'envelope') : ?>
-					<?php $filelist = dol_dir_list($upload_dir . '/' . $object->element . '/' . $object->ref);
-					if (!empty($filelist)) {
-						$file = array_shift($filelist);
-						$fileurl = $file['fullname'];
-						$filename = $file['name'];
-						$envelope_file = $ecmfile;
-						$envelope_file->fetch(0, '', 'doliletter/envelope/'.$object->ref.'/'.$filename, '', '', 'doliletter_envelope', $object->id);
-					}
-					?>
-					<strong class="grid-align-middle"><?php echo $langs->trans("LinkedDocument"); ?></strong>
-					<a href="<?php echo './../../../../document.php?hashp=' . $envelope_file->share ?>">
-						<span class="wpeo-button button-primary button-radius-2 grid-align-right"><i class="button-icon fas fa-file-pdf"></i><?php echo '  ' . $langs->trans('ShowDocument'); ?></span>
-					</a>
+						<?php $filelist = dol_dir_list($upload_dir . '/' . $object->element . '/' . $object->ref);
+						if (!empty($filelist)) {
+							$file = array_shift($filelist);
+							$fileurl = $file['fullname'];
+							$filename = $file['name'];
+							$envelope_file = $ecmfile;
+							$envelope_file->fetch(0, '', 'doliletter/envelope/'.$object->ref.'/'.$filename, '', '', 'doliletter_envelope', $object->id);
+						}
+						?>
+						<?php if(dol_strlen($envelope_file->share)) : ?>
+							<strong class="grid-align-middle"><?php echo $langs->trans("LinkedDocument"); ?></strong>
+							<a href="<?php echo './../../../../document.php?hashp=' . $envelope_file->share ?>">
+								<span class="wpeo-button button-primary button-radius-2 grid-align-right"><i class="button-icon fas fa-file-pdf"></i><?php echo '  ' . $langs->trans('ShowDocument'); ?></span>
+							</a>
+						<?php endif; ?>
 					<?php endif; ?>
 				</div>
 			<?php endif; ?>
